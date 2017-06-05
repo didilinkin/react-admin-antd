1a3eed6c076a4d34d4e6115fd55cd792dd8a7efa
yanxiaodi <929213769@qq.com>
2017-06-05 16:32:10 +0800

    :bug test:

--- src/components/Tables/tables2.jsx
+++ src/components/Tables/tables2.jsx
@@ -4,13 +4,28 @@ import { createStore } from 'redux'
 import { Provider, connect } from 'react-redux'
 import axios from 'axios'
 
-const getget = (url) => axios.get(url).then(function (response) {
-    debugger
-    return response.data
-}).catch(function (error) {
-    debugger
-    console.log(error)
-})
+// const getget = (url) => axios.get(url).then(function (response) {
+//     // debugger
+//     // console.log(response.data)
+//     return response.data
+// }).catch(function (error) {
+//     debugger
+//     console.log(error)
+// })
+
+const getget = (url) => {
+    return new Promise ( function (resolve, reject) {
+        axios.get(url)
+        .then ( response => {
+            let resulData = response.data
+            resolve ( resulData )
+        })
+        .catch ( error => {
+            reject ( error )
+        })
+    })
+}
+
 
 // React component
 class Counter extends Component {
@@ -60,8 +75,21 @@ const store = createStore(counter)
 // Map Redux state to component props
 function mapStateToProps (state, ownProps) {
     function handleDelete (id) {
-        getget('http://127.0.0.1:18082/ceshi')
-        debugger
+        // getget('http://127.0.0.1:18082/ceshi')
+        // let data = async function() { getget('/npm.json') }
+        // console.log(data)
+        // debugger
+
+        const asyncGet = async function () {
+            try {
+                let result = await getget('/npm.json')
+                console.log(result)
+            } catch(err) {
+                console.log(err)
+            }
+        }
+
+        asyncGet()
     }
 
 
