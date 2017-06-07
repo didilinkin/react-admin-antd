87ab92eda033eea2606fb0686d2c7afab0bbe9e4
yanxiaodi <929213769@qq.com>
2017-06-07 12:11:43 +0800

    :art: 添加'仓库管理' 页面; 更新路由配置; 测试通过

---
+++ src/views/Warehouse/pages/MaterialManagement.jsx
@@ -0,0 +1,14 @@
+// 材料管理
+import React from 'react'
+
+class MaterialManagement extends React.Component {
+    render () {
+        return (
+            <div>
+                <h1> 材料管理 </h1>
+            </div>
+        )
+    }
+}
+
+export default MaterialManagement
