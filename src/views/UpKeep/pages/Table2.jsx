42fd161e23838088191ca7829ba2963ed7e235ce
yanxiaodi <929213769@qq.com>
2017-06-08 15:32:47 +0800

    :bug: 更改'ESLint'配置 => 修改 switchCase缩进配置

--- src/views/UpKeep/pages/Table2.jsx
+++ src/views/UpKeep/pages/Table2.jsx
@@ -50,22 +50,22 @@ class Counter extends Component {
 function counter (state = {count: []}, action) {
     debugger
     switch (action.type) {
-    case 'increase':
-        return {count: action.payload}
-    default:
-        return {
-            count: [{
-                key: '1',
-                name: '胡彦斌',
-                age: 32,
-                address: '西湖区湖底公园1号'
-            }, {
-                key: '2',
-                name: '胡彦祖',
-                age: 42,
-                address: '西湖区湖底公园1号'
-            }]
-        }
+        case 'increase':
+            return {count: action.payload}
+        default:
+            return {
+                count: [{
+                    key: '1',
+                    name: '胡彦斌',
+                    age: 32,
+                    address: '西湖区湖底公园1号'
+                }, {
+                    key: '2',
+                    name: '胡彦祖',
+                    age: 42,
+                    address: '西湖区湖底公园1号'
+                }]
+            }
     }
 }
 
--- src/views/common/pages/HomeRouter.jsx
+++ src/views/common/pages/HomeRouter.jsx
@@ -1,4 +1,4 @@
-// 主页模版
+// 首页
 import React from 'react'
 // 测试antd
 import { Button }   from 'antd'
