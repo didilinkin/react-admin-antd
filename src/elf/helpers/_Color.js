55b15903e452a2a7e5eab930e816793f16039fdd
yanxiaodi <929213769@qq.com>
2017-06-14 18:14:26 +0800

    :lipstick: 将Vue项目中的 Sass库 'Elf'转换为ES版本; 测试成功; 添加'hotcss'方案(移动端布局终极解决方案) 测试成功; 样式管理全部完成

---
+++ src/elf/helpers/_Color.js
@@ -0,0 +1,13 @@
+// 颜色
+export const W = '#FFF'                 // 白 - 简写
+export const B = '#000'                 // 黑 - 简写
+
+export const base = '#F5F5F5'
+export const theme = '#108EE9'
+
+export const line = '#E3E3E3'           // 线
+
+// 提示色
+export const error = '#333'
+
+// 模块色
