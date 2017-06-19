658f4c598073329cb2fc2599c46f0104e079bcce
shangchaoadmin <admin@example.com>
2017-06-19 16:49:43 +0800

    删除多余包引用

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM     from 'react-dom'
 import SetRouter    from './router/index'
 import 'animate.css'
 
+alert(process.env.BABEL_ENV)
+
 ReactDOM.render(
     <SetRouter />,
     document.getElementById('root')
