d53b3bda6dc2d25633f527f55d5ffa07429b0142
shangchaoadmin <admin@example.com>
2017-06-19 15:35:47 +0800

    修改axios baseurl

--- src/views/Warehouse/pages/InventoryManage.jsx
+++ src/views/Warehouse/pages/InventoryManage.jsx
@@ -12,7 +12,7 @@ const asyncTest = async function () {
     try {
         // 内网测试
         let result = await apiPost(
-            'http://192.168.1.250:18082/upkeep/list',
+            'upkeep/list',
             { 'entryName': '灯泡2' }
         )
 
