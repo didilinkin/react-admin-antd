0f0352d499348aea10bf13c2963c2836695fd2c9
yanxiaodi <929213769@qq.com>
2017-06-20 14:23:22 +0800

    :art: 修复冲突BUG完成

--- src/views/Repair/pages/common/TableAddUp.jsx
+++ src/views/Repair/pages/common/TableAddUp.jsx
@@ -215,21 +215,18 @@ class TableAddUp extends React.Component {
                         </Row>
                         <Row>
                             <Col span={12}>
-<<<<<<< HEAD
                                 <FormItem label="所属楼宇" labelCol={{ span: 5 }}
                                     wrapperCol={{ span: 15 }}
                                 >
                                     <Input {...getFieldProps('buildName')} />
                                     <Input type="hidden" {...getFieldProps('buildId')} />
                                 </FormItem>
-=======
-                        <FormItem label="所属楼宇" labelCol={{ span: 5 }}
-                                  wrapperCol={{ span: 15 }}
-                        >
-                            <Input disabled="true" {...getFieldProps('buildName')} />
-                            <Input type="hidden" {...getFieldProps('buildId')} />
-                        </FormItem>
->>>>>>> 97a78c07abea56e39d12bf7ea6371cd5b153e2cb
+                                <FormItem label="所属楼宇" labelCol={{ span: 5 }}
+                                    wrapperCol={{ span: 15 }}
+                                >
+                                    <Input disabled="true" {...getFieldProps('buildName')} />
+                                    <Input type="hidden" {...getFieldProps('buildId')} />
+                                </FormItem>
                             </Col>
                             <Col span={12}>
                                 <FormItem label="报修单号" labelCol={{ span: 5 }}
