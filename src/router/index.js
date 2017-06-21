324e81a00930da0be1da7b1ee0d2aa42f6ec46ce
yanxiaodi <929213769@qq.com>
2017-06-21 18:05:41 +0800

    :bug: 修改路由

--- src/router/index.js
+++ src/router/index.js
@@ -7,46 +7,61 @@ import {
     Redirect
 } from 'react-router-dom'
 
-import {
-    routes,
-    RouteWithSubRoutes
-} from './HomeRouter'                                                               // 业务内容 路由匹配
+import styled               from 'styled-components'
 
-import styled               from 'styled-components'                                // 引入 styled样式配置
-
+import HomeTemplate         from '../views/common/pages/HomeTemplate'
 import NotFound             from '../views/common/pages/404'
 import Login                from '../views/common/pages/Login'
-import Registration         from '../views/common/pages/Registration'
 
 // 容器Div 样式配置( 不需要单独写一个组件 )
 const ContainerDiv = styled.section `
     height: 100vh;
 `
 
+// 伪造验证
+const fakeAuth = {
+    isAuthenticated: false
+
+    // ,
+    // // 登录
+    // authenticate(cb) {
+    //     this.isAuthenticated = true
+    //     setTimeout(cb, 100) // 模拟异步。
+    // }
+    // ,
+    // 退出帐户
+    // signout(cb) {
+    //     this.isAuthenticated = false
+    //     setTimeout(cb, 100)
+    // }
+}
+
+// 私有路由
+const AuthRouter = ({ component: Component, ...rest }) => (
+    <Route {...rest} render={props => (
+        fakeAuth.isAuthenticated ? (
+            <Component {...props} />
+        ) : (
+            <Redirect to={{
+                pathname: '/login',
+                state: { from: props.location } }}
+            />
+        ))}
+    />
+)
+
 const SetRouter = () => (
     <Router>
         <ContainerDiv>
             {/* 正确配置 - 静态下存在问题 - 在排查中 */}
             <Switch>
-                <Redirect exact from="/" to="/home/index" />
-                {routes.map((route, i) => (
-                    <RouteWithSubRoutes key={i} {...route} />
-                ))}
-
                 <Route path="/login" component={ Login } />
-                <Route path="/registration" component={ Registration } />
+
+                {/* 权限认证 => 必须登录 才能查看 */}
+                <AuthRouter path="/" component={ HomeTemplate } />
+
                 <Route component={ NotFound } />
             </Switch>
-
-            {/* 测试: 排除 404检查 - 测试成功 */}
-            {/*
-             <Redirect exact from="/" to="/home/index" />
-             {routes.map((route, i) => (
-             <RouteWithSubRoutes key={i} {...route} />
-             ))}
-             <Route path="/login" component={ Login } />
-             <Route path="/registration" component={ Registration } />
-            */}
         </ContainerDiv>
     </Router>
 )
