fb3b7491fe3bcbdfbc5cc6380c536d4c9e4a4c90
yanxiaodi <929213769@qq.com>
2017-06-22 18:02:31 +0800

    :art: Redux成功

--- src/index.js
+++ src/index.js
@@ -1,11 +1,26 @@
-import React        from 'react'
-import ReactDOM     from 'react-dom'
-import SetRouter    from './router/index'
+import React                from 'react'
+import ReactDOM             from 'react-dom'
+
+import { createStore }      from 'redux'
+import pmsApp               from './store/reducers'
+
+import { Counter }          from './router/index'
+
 import 'animate.css'
 
 // alert(process.env.BABEL_ENV)
 
-ReactDOM.render(
-    <SetRouter />,
-    document.getElementById('root')
+const store = createStore(pmsApp)
+const rootElement = document.getElementById('root')
+
+const render = () => ReactDOM.render(
+    <Counter
+        value={ store.getState() }
+        onIncrement={ () => store.dispatch({ type: 'INCREMENT' }) }
+        onDecrement={ () => store.dispatch({ type: 'DECREMENT' }) }
+    />,
+    rootElement
 )
+
+render()
+store.subscribe(render)
--- src/router/index.js
+++ src/router/index.js
@@ -1,69 +1,127 @@
 // 路由配置
 import React from 'react'
 import {
-    BrowserRouter as Router,
-    Route,
-    Switch,
-    Redirect
+    BrowserRouter as Router
+    // ,
+    // Route,
+    // Switch,
+    // Redirect
 } from 'react-router-dom'
+import PropTypes from 'prop-types'
 
 import styled               from 'styled-components'
 
-import HomeTemplate         from '../views/common/pages/HomeTemplate'
-import NotFound             from '../views/common/pages/404'
-import Login                from '../views/common/pages/Login'
+// import HomeTemplate         from '../views/common/pages/HomeTemplate'
+// import NotFound             from '../views/common/pages/404'
+// import Login                from '../views/common/pages/Login'
 
 // 容器Div 样式配置( 不需要单独写一个组件 )
-const ContainerDiv = styled.section `
+const RouterDiv = styled.section `
     height: 100vh;
 `
 
-// 伪造验证
-const fakeAuth = {
-    isAuthenticated: false
+// 测试 Redux
+class Counter extends React.Component {
+    constructor (props) {
+        super(props)
+        this.incrementAsync = this.incrementAsync.bind(this)
+        this.incrementIfOdd = this.incrementIfOdd.bind(this)
+    }
 
-    // ,
-    // // 登录
-    // authenticate(cb) {
-    //     this.isAuthenticated = true
-    //     setTimeout(cb, 100) // 模拟异步。
-    // }
-    // ,
-    // 退出帐户
-    // signout(cb) {
-    //     this.isAuthenticated = false
-    //     setTimeout(cb, 100)
-    // }
+    incrementIfOdd () {
+        if (this.props.value % 2 !== 0) {
+            this.props.onIncrement()
+        }
+    }
+
+    incrementAsync () {
+        setTimeout(this.props.onIncrement, 1000)
+    }
+
+    render () {
+        const { value, onIncrement, onDecrement } = this.props
+        return (
+            <p>
+                Clicked: {value} times
+                {' '}
+
+                <button onClick={onIncrement}>
+                    +
+                </button>
+                {' '}
+
+                <button onClick={onDecrement}>
+                    -
+                </button>
+                {' '}
+
+                <button onClick={this.incrementIfOdd}>
+                    增量奇数( 是偶数则不增加; 奇数增加 )
+                </button>
+                {' '}
+
+                <button onClick={this.incrementAsync}>
+                    异步增加
+                </button>
+            </p>
+        )
+    }
 }
 
+// 对 Redux 对象 Counter设置 propTypes
+Counter.propTypes = {
+    value: PropTypes.number.isRequired,
+    onIncrement: PropTypes.func.isRequired,
+    onDecrement: PropTypes.func.isRequired
+}
+
+// 伪造验证
+// const fakeAuth = {
+//     isAuthenticated: false
+
+//     // ,
+//     // // 登录
+//     // authenticate(cb) {
+//     //     this.isAuthenticated = true
+//     //     setTimeout(cb, 100) // 模拟异步。
+//     // }
+//     // ,
+//     // 退出帐户
+//     // signout(cb) {
+//     //     this.isAuthenticated = false
+//     //     setTimeout(cb, 100)
+//     // }
+// }
+
 // 私有路由
-const AuthRouter = ({ component: Component, ...rest }) => (
-    <Route {...rest} render={props => (
-        fakeAuth.isAuthenticated ? (
-            <Component {...props} />
-        ) : (
-            <Redirect to={{
-                pathname: '/login',
-                state: { from: props.location } }}
-            />
-        ))}
-    />
-)
+// const AuthRouter = ({ component: Component, ...rest }) => (
+//     <Route {...rest} render={props => (
+//         fakeAuth.isAuthenticated ? (
+//             <Component {...props} />
+//         ) : (
+//             <Redirect to={{
+//                 pathname: '/login',
+//                 state: { from: props.location } }}
+//             />
+//         ))}
+//     />
+// )
 
 const SetRouter = () => (
     <Router>
-        <ContainerDiv>
-            {/* 正确配置 - 静态下存在问题 - 在排查中 */}
-            <Switch>
-                <Route path="/login" component={ Login } />
+        <RouterDiv>
+            {/* 测试Redux => 原始路由配置 暂时注释 */}
+            {/*
+                <Switch>
+                    <Route path="/login" component={ Login } />
 
-                {/* 权限认证 => 必须登录 才能查看 */}
-                <AuthRouter path="/" component={ HomeTemplate } />
+                    <AuthRouter path="/" component={ HomeTemplate } />
 
-                <Route component={ NotFound } />
-            </Switch>
-        </ContainerDiv>
+                    <Route component={ NotFound } />
+                </Switch>
+            */}
+        </RouterDiv>
     </Router>
 )
 
-export default SetRouter
+export { SetRouter, Counter }
