d2014b4226358324d6dc2a507a80ada7ba6110a2
liuzhiqiang <liuzhiqiang@forevercj.com>
2017-06-26 11:00:04 +0800

    解决多选问题

--- src/views/Repair/pages/common/RectificationAddUp.jsx
+++ src/views/Repair/pages/common/RectificationAddUp.jsx
@@ -64,7 +64,7 @@ class RectificationAddUp extends React.Component {
                     buildName: resulData.data.buildName,
                     buildId: resulData.data.buildId,
                     clientName: resulData.data.clientName,
-                    clientNameOne: resulData.data.clientName,
+                    clientNameOne: resulData.data.clientName + '(' + resulData.data.roomNums + ')',
                     clientType: resulData.data.clientType,
                     clientId: resulData.data.clientId,
                     roomNums: resulData.data.roomNums,
@@ -255,7 +255,7 @@ class RectificationAddUp extends React.Component {
                                         >
                                             {this.state.clientList.map(d => {
                                                 let key = d.clientId + ':' + d.roomNum + ':' + d.clientType
-                                                return <Option key={key}>{d.clientName}</Option>
+                                                return <Option key={key}>{d.clientName + '(' + d.roomNum + ')'}</Option>
                                             })}
                                         </Select>
                                     )}
