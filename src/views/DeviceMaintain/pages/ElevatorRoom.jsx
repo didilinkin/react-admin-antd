f353f83539a4cd98daa36e2dcbde8e518711fb03
yanxiaodi <929213769@qq.com>
2017-07-04 14:47:55 +0800

    :heavy_plus_sign: 添加'设备维护-设备巡检-电梯系统'的所有4级页面(电梯机房 与 日常检查)

--- src/views/DeviceMaintain/pages/ElevatorRoom.jsx
+++ src/views/DeviceMaintain/pages/ElevatorRoom.jsx
@@ -1,11 +1,11 @@
-// 设备维护 - 设备巡检 - 电梯间系统
+// 设备维护 - 设备巡检 - 电梯系统 - 电梯机房
 import React from 'react'
 
 class ElevatorRoom extends React.Component {
     render () {
         return (
             <div>
-                <h1> 设备维护 - 设备巡检 - 电梯间系统 </h1>
+                <h1> 设备维护 - 设备巡检 - 电梯系统 - 电梯机房 </h1>
             </div>
         )
     }
