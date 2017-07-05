10dd147a0e232a2f5c4741e27801e5ee94e4f3a0
yanxiaodi <929213769@qq.com>
2017-07-05 17:51:21 +0800

    :bug: 正在配置新路由(Browser)

---
+++ src/index.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { render } from 'react-dom'
+
+import { createStore, applyMiddleware } from 'redux'
+import { Provider }                     from 'react-redux'          // 全局store
+import { createLogger }                 from 'redux-logger'         // 开发阶段: 打印redux 日志
+import thunk                            from 'redux-thunk'          // 异步
+
+import rootReducer                      from './store/reducers'     // 根reducers
+
+import routes                           from './router'
+
+const rootElement = document.getElementById('root')
+
+// 开发环境下, 打印 redux日志
+const middleware = [ thunk ]
+if (process.env.NODE_ENV !== 'production') {
+    middleware.push(createLogger())
+}
+
+// 根store
+const store = createStore(
+    rootReducer,
+    applyMiddleware(...middleware)
+)
+
+render(
+    <Provider store={store}>
+        { routes }
+    </Provider>,
+    rootElement
+)
---
+++ src/router/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import {
+    BrowserRouter as Router,
+    Route
+} from 'react-router-dom'
+
+
+export default routes
