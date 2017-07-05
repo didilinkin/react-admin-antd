7a47277637e8bfa23c4610f3b82abe165a5adf8b
yanxiaodi <929213769@qq.com>
2017-07-05 11:16:25 +0800

    :bug: 添加'设备巡检-电器系统'下的所有4级页面(暂无侧导航栏导航, 只能通过URL访问)

---
+++ src/views/DeviceMaintain/pages/WeakRoom.jsx
@@ -0,0 +1,14 @@
+// 设备维护 - 设备巡检 - 电器系统 - 弱电间巡查记录
+import React from 'react'
+
+class WeakRoom extends React.Component {
+    render () {
+        return (
+            <div>
+                <h1> 设备维护 - 设备巡检 - 电器系统 - 弱电间巡查记录 </h1>
+            </div>
+        )
+    }
+}
+
+export default WeakRoom
