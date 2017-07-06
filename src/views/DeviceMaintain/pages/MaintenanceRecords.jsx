4e3602b6046e6cf6e6985a589ff5b25111efebdf
yanxiaodi <929213769@qq.com>
2017-07-06 12:59:41 +0800

    :heavy_plus_sign: 侧导航栏添加'水暖系统'与'消防系统'所有4级页面导航; 添加'消防系统'全部页面, 并添加路由配置; 全部测试通过;

---
+++ src/views/DeviceMaintain/pages/MaintenanceRecords.jsx
@@ -0,0 +1,14 @@
+// 设备维护 - 设备巡检 - 消防系统 - 消防维保记录
+import React from 'react'
+
+class MaintenanceRecords extends React.Component {
+    render () {
+        return (
+            <div>
+                <h1> 设备维护 - 设备巡检 - 消防系统 - 消防维保记录 </h1>
+            </div>
+        )
+    }
+}
+
+export default MaintenanceRecords
---
+++ src/views/DeviceMaintain/pages/WaterTank.jsx
@@ -0,0 +1,14 @@
+// 设备维护 - 设备巡检 - 消防系统 - 高位消防水箱
+import React from 'react'
+
+class WaterTank extends React.Component {
+    render () {
+        return (
+            <div>
+                <h1> 设备维护 - 设备巡检 - 消防系统 - 高位消防水箱 </h1>
+            </div>
+        )
+    }
+}
+
+export default WaterTank
