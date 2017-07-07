ceacb9ad2c4f9b31cec8bedae8acc71847274121
liuzhiqiang <liuzhiqiang@forevercj.com>
2017-07-07 14:39:55 +0800

    异常处理

--- src/views/Repair/pages/RepairList.jsx
+++ src/views/Repair/pages/RepairList.jsx
@@ -239,7 +239,7 @@ class RepairList extends Component {
     endDate = ''
     getDate = (date, dateString) => {
         this.startDate = dateString[0]
-        this.endDate = dateString[1]
+        this.endDate = dateString[1] + ' 23:59:59'
     }
     render () {
         return (
