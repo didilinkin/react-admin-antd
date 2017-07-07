e322c457bd19f543dcfbb992c1a233ec0a4d331d
liuzhiqiang <liuzhiqiang@forevercj.com>
2017-07-07 17:22:36 +0800

    删除workspace

--- src/views/test/Echarts.jsx
+++ src/views/test/Echarts.jsx
@@ -1,59 +1,71 @@
-// echarts-react 测试组件
-import React from 'react'
-import ReactEcharts from 'echarts-for-react'
-import { PieChart, Pie, Cell } from 'recharts'
-const data = [
-    {
-        name: 'Group A',
-        value: 400
-    }, {
-        name: 'Group B',
-        value: 300
-    }, {
-        name: 'Group C',
-        value: 300
-    }, {
-        name: 'Group D',
-        value: 200
-    }
-]
-const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042']
-const RADIAN = Math.PI / 180
-const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent, index }) => {
-    const radius = innerRadius + (outerRadius - innerRadius) * 0.5
-    const x = cx + radius * Math.cos(-midAngle * RADIAN)
-    const y = cy + radius * Math.sin(-midAngle * RADIAN)
+import React, { Component } from 'react'
 
-    return (
-        <text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central">
-            {`${(percent * 100).toFixed(0)}%`}
-        </text>
-    )
-}
+// 引入 ECharts 主模块
+import echarts from 'echarts/lib/echarts'
+// 引入柱状图
+import  'echarts/lib/chart/bar'
+import  'echarts/lib/chart/pie'
+// 引入提示框和标题组件
+import 'echarts/lib/component/tooltip'
+import 'echarts/lib/component/title'
 
-class Echarts extends React.Component {
-    getOtion = () => {
-        const option = {
-            title: {
-                text: '客户评价情况',
-                subtext: ' ',
-                x: 'center'
+class EchartsTest extends Component {
+    componentDidMount () {
+        // 基于准备好的dom，初始化echarts实例
+        let myChart = echarts.init(document.getElementById('main'))
+        // 绘制图表
+        myChart.setOption({
+            title: { text: 'ECharts 入门示例' },
+            tooltip: {},
+            xAxis: {
+                data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
             },
+            yAxis: {},
+            series: [{
+                name: '销量',
+                type: 'bar',
+                data: [5, 20, 36, 10, 10, 20]
+            }]
+        })
+
+        // 基于准备好的dom，初始化echarts实例
+        let myChart1 = echarts.init(document.getElementById('main1'))
+        // 绘制图表
+        myChart1.setOption({
             tooltip: {
                 trigger: 'item',
-                formatter: '{a} <br/>{b} : {c} ({d}%)'
+                formatter: '{a} <br/>{b}: {c} ({d}%)'
             },
             legend: {
+                show: true,
                 orient: 'vertical',
-                left: 'left',
+                left: '20px',
                 data: ['直接访问', '邮件营销', '联盟广告', '视频广告', '搜索引擎']
             },
             series: [
                 {
                     name: '访问来源',
                     type: 'pie',
-                    radius: '55%',
-                    center: ['50%', '60%'],
+                    radius: ['50%', '70%'],
+                    avoidLabelOverlap: false,
+                    label: {
+                        normal: {
+                            show: false,
+                            position: 'center'
+                        },
+                        emphasis: {
+                            show: true,
+                            textStyle: {
+                                fontSize: '30',
+                                fontWeight: 'bold'
+                            }
+                        }
+                    },
+                    labelLine: {
+                        normal: {
+                            show: false
+                        }
+                    },
                     data: [
                         {value: 335,
                             name: '直接访问'},
@@ -65,67 +77,23 @@ class Echarts extends React.Component {
                             name: '视频广告'},
                         {value: 1548,
                             name: '搜索引擎'}
-                    ],
-                    itemStyle: {
-                        emphasis: {
-                            shadowBlur: 10,
-                            shadowOffsetX: 0,
-                            shadowColor: 'rgba(0, 0, 0, 0.5)'
-                        }
-                    }
+                    ]
                 }
             ]
-        }
-        return option
+        })
     }
-    onChartClick = () => function (param, echart) {
-        console.log(param, echart)
-        alert('chart click')
-    }
-    onChartLegendselectchanged = () => function (param, echart) {
-        console.log(param, echart)
-        alert('chart legendselectchanged')
-    }
-    onChartReady = () => function (echart) {
-        console.log('echart is ready', echart)
-    }
-
     render () {
         return (
-            <div className="examples">
-                <div style={{'float': 'left'}}>
-                    <h2 style={{ 'lineHeight': 'normal',
-                        'fontSize': '18px',
-                        'color': '#333'}}>test</h2>
-                    <PieChart width={ 400 } height={ 400 } onMouseEnter={this.onPieEnter}>
-                        <Pie
-                            data={ data }
-                            cx={ 130 }
-                            cy={ 190 }
-                            labelLine={ false }
-                            label={ renderCustomizedLabel }
-                            outerRadius={ 100 }
-                            fill="#8884d8"
-                        >
-                            {
-                                data.map((entry, index) => <Cell fill={COLORS[index % COLORS.length]} />)
-                            }
-                        </Pie>
-                    </PieChart>
-                </div>
-                <div className="parent" style={{'float': 'left'}}>
-                    <ReactEcharts
-                        option={ this.getOtion() }
-                        style={{
-                            height: '350px',
-                            width: '350px'
-                        }}
-                        className="react_for_echarts"
-                    />
-                </div>
+            <div>
+                <div id="main1" style={{ width: 400,
+                    height: 400 }}
+                />
+                <div id="main" style={{ width: 400,
+                    height: 400 }}
+                />
             </div>
         )
     }
 }
 
-export default Echarts
+export default EchartsTest
