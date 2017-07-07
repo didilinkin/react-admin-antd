72caf454095c521889381bff46d7e8d53b0fa2ae
songjifei <songjifei@forevercj.com>
2017-07-07 15:19:45 +0800

    test

--- src/views/test/Echarts.jsx
+++ src/views/test/Echarts.jsx
@@ -1,6 +1,35 @@
 // echarts-react 测试组件
 import React from 'react'
 import ReactEcharts from 'echarts-for-react'
+import { PieChart, Pie, Cell } from 'recharts'
+const data = [
+    {
+        name: 'Group A',
+        value: 400
+    }, {
+        name: 'Group B',
+        value: 300
+    }, {
+        name: 'Group C',
+        value: 300
+    }, {
+        name: 'Group D',
+        value: 200
+    }
+]
+const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042']
+const RADIAN = Math.PI / 180
+const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent, index }) => {
+    const radius = innerRadius + (outerRadius - innerRadius) * 0.5
+    const x = cx + radius * Math.cos(-midAngle * RADIAN)
+    const y = cy + radius * Math.sin(-midAngle * RADIAN)
+
+    return (
+        <text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central">
+            {`${(percent * 100).toFixed(0)}%`}
+        </text>
+    )
+}
 
 class Echarts extends React.Component {
     getOtion = () => {
@@ -64,7 +93,27 @@ class Echarts extends React.Component {
     render () {
         return (
             <div className="examples">
-                <div className="parent">
+                <div style={{'float': 'left'}}>
+                    <h2 style={{ 'lineHeight': 'normal',
+                        'fontSize': '18px',
+                        'color': '#333'}}>test</h2>
+                    <PieChart width={ 400 } height={ 400 } onMouseEnter={this.onPieEnter}>
+                        <Pie
+                            data={ data }
+                            cx={ 130 }
+                            cy={ 190 }
+                            labelLine={ false }
+                            label={ renderCustomizedLabel }
+                            outerRadius={ 100 }
+                            fill="#8884d8"
+                        >
+                            {
+                                data.map((entry, index) => <Cell fill={COLORS[index % COLORS.length]} />)
+                            }
+                        </Pie>
+                    </PieChart>
+                </div>
+                <div className="parent" style={{'float': 'left'}}>
                     <ReactEcharts
                         option={ this.getOtion() }
                         style={{
