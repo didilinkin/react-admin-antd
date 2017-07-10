5997812e3a4e54cf8e65ad6203fa61c39a445344
damon <limeng@forevercj.com>
2017-07-10 18:02:04 +0800

    水暖完成

--- src/views/DeviceMaintain/pages/HeatTransferStation.jsx
+++ src/views/DeviceMaintain/pages/HeatTransferStation.jsx
@@ -40,12 +40,12 @@ class HeatTransferStation extends Component {
                 dataIndex: 'createDate',
                 key: 'createDate'
             }, {
-                title: '设备编号',
+                title: '机房编号',
                 width: 150,
                 dataIndex: 'machineRoomNumber',
                 key: 'machineRoomNumber'
             }, {
-                title: '设备名称',
+                title: '机房名称',
                 width: 150,
                 dataIndex: 'machineRoomName',
                 key: 'machineRoomName'
@@ -55,35 +55,228 @@ class HeatTransferStation extends Component {
                 dataIndex: 'patrolName',
                 key: 'patrolName'
             }, {
-                title: '电压电流',
+                title: '巡查节点',
                 width: 150,
-                dataIndex: 'sprayPump',
-                key: 'sprayPump'
+                dataIndex: 'patrolNode',
+                key: 'patrolNode'
             }, {
-                title: '水泵、阀门漏水及阀门开关闭',
-                width: 150,
-                dataIndex: 'livingWaterPump',
-                key: 'livingWaterPump'
+                title: '高区板换',
+                children: [{
+                    title: '1#电机电流',
+                    dataIndex: 'currentOneH',
+                    key: 'currentOneH',
+                    width: 150
+                }, {
+                    title: '2#电机电流',
+                    dataIndex: 'currentTwoH',
+                    key: 'currentTwoH',
+                    width: 150
+                }, {
+                    title: '温度（一次）',
+                    width: 150,
+                    children: [{
+                        title: '进水℃',
+                        dataIndex: 'temperatureOneInH',
+                        key: 'temperatureOneInH',
+                        width: 150
+                    }, {
+                        title: '出水℃',
+                        dataIndex: 'temperatureOneOutH',
+                        key: 'temperatureOneOutH',
+                        width: 150
+                    }]
+                }, {
+                    title: '温度（二次）',
+                    width: 150,
+                    children: [{
+                        title: '进水℃',
+                        dataIndex: 'temperatureTwoInH',
+                        key: 'temperatureTwoInH',
+                        width: 150
+                    }, {
+                        title: '出水℃',
+                        dataIndex: 'temperatureTwoOutH',
+                        key: 'temperatureTwoOutH',
+                        width: 150
+                    }]
+                }, {
+                    title: '压力（一次）',
+                    width: 150,
+                    children: [{
+                        title: '进水Mpa',
+                        dataIndex: 'pressureOneInH',
+                        key: 'pressureOneInH',
+                        width: 150
+                    }, {
+                        title: '出水Mpa',
+                        dataIndex: 'pressureOneOutH',
+                        key: 'pressureOneOutH',
+                        width: 150
+                    }]
+                }, {
+                    title: '压力（二次）',
+                    width: 150,
+                    children: [{
+                        title: '进水Mpa',
+                        dataIndex: 'pressureTwoInH',
+                        key: 'pressureTwoInH',
+                        width: 150
+                    }, {
+                        title: '出水Mpa',
+                        dataIndex: 'pressureTwoOutH',
+                        key: 'pressureTwoOutH',
+                        width: 150
+                    }]
+                }]
             }, {
-                title: '水箱水位',
-                width: 150,
-                dataIndex: 'firePumpUnit',
-                key: 'firePumpUnit'
+                title: '低区板换',
+                children: [{
+                    title: '1#电机电流',
+                    dataIndex: 'currentOneL',
+                    key: 'currentOneL',
+                    width: 150
+                }, {
+                    title: '2#电机电流',
+                    dataIndex: 'currentTwoL',
+                    key: 'currentTwoL',
+                    width: 150
+                }, {
+                    title: '温度（一次）',
+                    width: 150,
+                    children: [{
+                        title: '进水℃',
+                        dataIndex: 'temperatureOneInL',
+                        key: 'temperatureOneInL',
+                        width: 150
+                    }, {
+                        title: '出水℃',
+                        dataIndex: 'temperatureOneOutL',
+                        key: 'temperatureOneOutL',
+                        width: 150
+                    }]
+                }, {
+                    title: '温度（二次）',
+                    width: 150,
+                    children: [{
+                        title: '进水℃',
+                        dataIndex: 'temperatureTwoInL',
+                        key: 'temperatureTwoInL',
+                        width: 150
+                    }, {
+                        title: '出水℃',
+                        dataIndex: 'temperatureTwoOutL',
+                        key: 'temperatureTwoOutL',
+                        width: 150
+                    }]
+                }, {
+                    title: '压力（一次）',
+                    width: 150,
+                    children: [{
+                        title: '进水Mpa',
+                        dataIndex: 'pressureOneInL',
+                        key: 'pressureOneInL',
+                        width: 150
+                    }, {
+                        title: '出水Mpa',
+                        dataIndex: 'pressureOneOutL',
+                        key: 'pressureOneOutL',
+                        width: 150
+                    }]
+                }, {
+                    title: '压力（二次）',
+                    width: 150,
+                    children: [{
+                        title: '进水Mpa',
+                        dataIndex: 'pressureTwoInL',
+                        key: 'pressureTwoInL',
+                        width: 150
+                    }, {
+                        title: '出水Mpa',
+                        dataIndex: 'pressureTwoOutL',
+                        key: 'pressureTwoOutL',
+                        width: 150
+                    }]
+                }]
             }, {
-                title: '供水压力',
-                width: 150,
-                dataIndex: 'HeatTransferStationGroup',
-                key: 'HeatTransferStationGroup'
+                title: '高区集水器',
+                children: [{
+                    title: '进水Mpa',
+                    dataIndex: 'collectorInH',
+                    key: 'collectorInH',
+                    width: 150
+                }, {
+                    title: '出水Mpa',
+                    dataIndex: 'collectorOutH',
+                    key: 'collectorOutH',
+                    width: 150
+                }]
             }, {
-                title: '各种仪表指示显示与标示',
-                width: 150,
-                dataIndex: 'topFloor',
-                key: 'topFloor'
+                title: '低区集水器',
+                children: [{
+                    title: '进水Mpa',
+                    dataIndex: 'collectorInL',
+                    key: 'collectorInL',
+                    width: 150
+                }, {
+                    title: '出水Mpa',
+                    dataIndex: 'collectorOutL',
+                    key: 'collectorOutL',
+                    width: 150
+                }]
             }, {
-                title: '各水泵运行状况',
-                width: 150,
-                dataIndex: 'coolingTower',
-                key: 'coolingTower'
+                title: '累计热量',
+                children: [{
+                    title: '开机热量',
+                    dataIndex: 'startHeat',
+                    key: 'startHeat',
+                    width: 150
+                }, {
+                    title: '关机热量',
+                    dataIndex: 'shutdownHeat',
+                    key: 'shutdownHeat',
+                    width: 150
+                }, {
+                    title: '合计热量',
+                    dataIndex: 'sumHeat',
+                    key: 'sumHeat',
+                    width: 150
+                }]
+            }, {
+                title: '高区换热Kwh',
+                children: [{
+                    title: '开机电量',
+                    dataIndex: 'startPowerH',
+                    key: 'startPowerH',
+                    width: 150
+                }, {
+                    title: '关机电量',
+                    dataIndex: 'shutdownPowerH',
+                    key: 'shutdownPowerH',
+                    width: 150
+                }, {
+                    title: '合计电量',
+                    dataIndex: 'sumPowerH',
+                    key: 'sumPowerH',
+                    width: 150
+                }]
+            }, {
+                title: '低区换热Kwh',
+                children: [{
+                    title: '开机电量',
+                    dataIndex: 'startPowerL',
+                    key: 'startPowerL',
+                    width: 150
+                }, {
+                    title: '关机电量',
+                    dataIndex: 'shutdownPowerL',
+                    key: 'shutdownPowerL',
+                    width: 150
+                }, {
+                    title: '合计电量',
+                    dataIndex: 'sumPowerL',
+                    key: 'sumPowerL',
+                    width: 150
+                }]
             }, {
                 title: '异常情况',
                 width: 200,
@@ -155,7 +348,7 @@ class HeatTransferStation extends Component {
                 </span>
                 <Spin spinning={this.state.loading}>
                     <Table
-                        scroll={{ x: 1788 }}
+                        scroll={{ x: 5000 }}
                         bordered={3}
                         dataSource={this.state.dataSource}
                         columns={this.state.columns}
