00c081d90a64b9b8a5ed4d062710d299d1fd0dc9
liuzhiqiang <liuzhiqiang@forevercj.com>
2017-07-11 17:04:41 +0800

    删除workspace

--- src/views/Repair/pages/Details/CorrectionDetail.jsx
+++ src/views/Repair/pages/Details/CorrectionDetail.jsx
@@ -1,6 +1,6 @@
 // 整改信息明细
 import React from 'react'
-import { Row, Col } from 'antd'
+import { Row, Col, Button } from 'antd'
 import '../../../../style/test.less'
 import { apiPost, baseURL } from '../../../../api'
 
@@ -38,34 +38,44 @@ class App extends React.Component {
     componentWillMount () {
         this.initialRemarks()
     }
+    preview = () => {
+        window.document.body.innerHTML = document.getElementById('box2').innerHTML
+        window.print()
+        window.location.reload()
+    }
     render () {
         return (
-            <div className="box1">
-                <h2>整改信息明细</h2>
-                <Row>
-                    <Col span={12}><b>检查日期：</b> {this.state.data.inspectDate}</Col>
-                    <Col span={12}><b>所属楼宇：</b>{this.state.data.buildName}</Col>
-                </Row>
-                <Row>
-                    <Col span={12}><b>公司名称：</b>{this.state.data.clientName}</Col>
-                    <Col span={12}><b>所属房间：</b>{this.state.data.roomNums}</Col>
-                </Row>
-                <Row>
-                    <Col span={24}><b>整改项目：</b>
-                        <div style={{float: 'left'}}>
-                            {this.state.data.rectificationContent}
-                        </div>
-                    </Col>
-                </Row>
-                <ul>
-                    <li>
-                        <b>现场图片：</b>
-                        {this.state.data.imgUrls}
-                    </li>
-                </ul>
-                <Row>
-                    <Col span={24}><b>检查人：</b> {this.state.data.inspectName}</Col>
-                </Row>
+            <div>
+                <div id="box2">
+                    <div id="box1" className="box1">
+                        <h2>整改信息明细</h2>
+                        <Row>
+                            <Col span={12}><b>检查日期：</b> {this.state.data.inspectDate}</Col>
+                            <Col span={12}><b>所属楼宇：</b>{this.state.data.buildName}</Col>
+                        </Row>
+                        <Row>
+                            <Col span={12}><b>公司名称：</b>{this.state.data.clientName}</Col>
+                            <Col span={12}><b>所属房间：</b>{this.state.data.roomNums}</Col>
+                        </Row>
+                        <Row>
+                            <Col span={24}><b>整改项目：</b>
+                                <div style={{float: 'left'}}>
+                                    {this.state.data.rectificationContent}
+                                </div>
+                            </Col>
+                        </Row>
+                        <ul>
+                            <li>
+                                <b>现场图片：</b>
+                                {this.state.data.imgUrls}
+                            </li>
+                        </ul>
+                        <Row>
+                            <Col span={24}><b>检查人：</b> {this.state.data.inspectName}</Col>
+                        </Row>
+                    </div>
+                </div>
+                <Button onClick={() => this.preview()}>打印</Button>
             </div>
         )
     }
