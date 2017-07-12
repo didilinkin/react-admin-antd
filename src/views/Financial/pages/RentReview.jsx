01b55dbf4af2f60507b341b9e558754e6ab47070
songjifei <songjifei@forevercj.com>
2017-07-12 15:55:27 +0800

    租金

--- src/views/Financial/pages/RentReview.jsx
+++ src/views/Financial/pages/RentReview.jsx
@@ -1,7 +1,7 @@
 // 财务管理 - 租金审核
 import React from 'react'
 import { Row, Col, Input } from 'antd'
-import '../../../../style/test.less'
+import '../../../style/test.less'
 
 
 class App extends React.Component {
