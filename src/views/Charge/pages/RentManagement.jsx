0d2f589b321d629a93bf6438dad0f16ee270fa64
yanxiaodi <929213769@qq.com>
2017-07-13 13:12:23 +0800

    :art: 添加'收费管理-租金管理'新页面; 路由测试完成;

---
+++ src/views/Charge/pages/RentManagement.jsx
@@ -0,0 +1,11 @@
+// 收费管理 - 租金管理
+import React from 'react'
+
+class RentManagement extends React.Component {
+    render () {
+        return (
+            <h1> 收费管理 - 租金管理 </h1>
+        )
+    }
+}
+export default RentManagement
