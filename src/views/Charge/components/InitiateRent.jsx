d65f4810f1e55ef6f2275c669f9a3997c0eac8a9
songjifei <songjifei@forevercj.com>
2017-07-14 11:23:02 +0800

    initiateRent

--- src/views/Charge/components/InitiateRent.jsx
+++ src/views/Charge/components/InitiateRent.jsx
@@ -1,6 +1,6 @@
 // 审核后
 import React from 'react'
-import { Row, Col, DatePicker, Input, Select } from 'antd'
+import { Row, Col, DatePicker, Input, Select, Button } from 'antd'
 import '../../../style/test.less'
 
 const Option = Select.Option
@@ -22,19 +22,19 @@ class App extends React.Component {
         return (
             <div className="contract">
                 <Row>
-                    <Col span={24}><b>本期周期：</b><DatePicker onChange={onChange} /></Col>
+                    <Col span={24}><i>本期周期：</i><DatePicker onChange={onChange} /></Col>
                 </Row>
                 <Row>
-                    <Col span={24}><b>收费期限：</b><DatePicker onChange={onChange1} /></Col>
+                    <Col span={24}><i>收费期限：</i><DatePicker onChange={onChange1} /></Col>
                 </Row>
                 <Row>
-                    <Col span={24}><b>本期租金：</b>¥ 50000.0 </Col>
+                    <Col span={24}><i>本期租金：</i>¥ 50000.0 </Col>
                 </Row>
                 <Row>
-                    <Col span={24}><b>优惠金额：</b><Input addonBefore="¥" defaultValue="请输入" /></Col>
+                    <Col span={24}><i>优惠金额：</i><Input addonBefore="¥" defaultValue="请输入" /></Col>
                 </Row>
                 <Row>
-                    <Col span={24}><b>付款账户：</b>
+                    <Col span={24}><i>付款账户：</i>
                         <Select defaultValue="lucy" style={{ width: '120' }} onChange={handleChange}>
                             <Option value="jack">Jack</Option>
                             <Option value="lucy">Lucy</Option>
@@ -43,6 +43,9 @@ class App extends React.Component {
                         </Select>
                     </Col>
                 </Row>
+                <p className="line" />
+                <Button type="primary">确认</Button>&nbsp;&nbsp;
+                <Button>取消</Button>
             </div>
         )
     }
