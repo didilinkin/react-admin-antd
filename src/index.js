02421ddb6efce2397df20f7776fa11a2614fd186
yanxiaodi <929213769@qq.com>
2017-07-14 15:48:02 +0800

    :construction_worker: 清空文件夹; 开始调整结构

--- src/index.js
+++ src/index.js
@@ -2,35 +2,7 @@
 import React from 'react'
 import { render } from 'react-dom'
 
-// Redux 状态
-import { Provider } from 'react-redux'
-import { ConnectedRouter } from 'react-router-redux'
-import store from './store/configureStore'
-
-// Router 路由
-import { Route } from 'react-router-dom'
-// import Routes from './router'                 // 根 路由配置
-
-// 临时测试 子页面
-import App from './containers/App'
-import Foo from './containers/Foo'
-import Bar from './containers/Bar'
-import Home from './containers/Home'
-
-const rootElement = document.getElementById('root')
-
 render(
-    <Provider store={ store }>
-        <div>
-            <ConnectedRouter history={ history }>
-                <div>
-                    <Route exact path="/" component={ App } />
-                    <Route path="/about" component={ Foo } />
-                    <Route path="/topics" component={ Bar } />
-                    <Route path="/home" component={ Home } />
-                </div>
-            </ConnectedRouter>
-        </div>
-    </Provider>,
-    rootElement
+    <div />,
+    document.getElementById('root')
 )
--- src/store/reducers/index.js
+++
@@ -1,9 +0,0 @@
-// reducers 根
-import { combineReducers } from 'redux'
-// import auth from './auth'
-import count from './count'
-
-export default combineReducers({
-    // auth,
-    count
-})
