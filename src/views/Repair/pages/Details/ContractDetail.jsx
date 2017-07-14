a723d9ddad07f28d2741b256a93e99cfa7afc2f3
songjifei <songjifei@forevercj.com>
2017-07-14 14:27:33 +0800

    合同详情/租赁合同详情

--- src/views/Repair/pages/Details/ContractDetail.jsx
+++ src/views/Repair/pages/Details/ContractDetail.jsx
@@ -103,47 +103,6 @@ class App extends React.Component {
                         </ul>
                     </div>
                 </div>
-
-                <div className="wrapbox">
-                    <div className="title">
-                        客户信息
-                    </div>
-                    <div className="main">
-                        <Row>
-                            <Col span={8}><b>租赁客户名称：</b>张三的公司</Col>
-                            <Col span={8}><b>联系人：</b>王小明</Col>
-                            <Col span={8}><b>经理电话：</b>123456789</Col>
-                        </Row>
-                        <Row>
-                            <Col span={8}><b>行政电话：</b>123456789 </Col>
-                            <Col span={8}><b>财务电话：</b>123456789</Col>
-                            <Col span={8}><b>E-mail：</b>123456789@123.com</Col>
-                        </Row>
-                        <Row>
-                            <Col span={8}><b>签约日期：</b>2017-7-11 15:23:19 </Col>
-                            <Col span={16} />
-                        </Row>
-                    </div>
-                </div>
-                <div className="wrapbox">
-                    <div className="title">
-                        日期信息
-                    </div>
-                    <div className="main">
-                        <Row>
-                            <Col span={8}><b>租赁周期：</b>2015-10-02  ~ 2015-10-10</Col>
-                            <Col span={16}><b>录入时间：</b>王小明      2016-09-26    12:12:12</Col>
-                        </Row>
-                        <Row>
-                            <Col span={8}><b>合同编号：</b>ABC-123456789 </Col>
-                            <Col span={16}><b>最后修改：</b>王小明      2016-09-26    12:12:12</Col>
-                        </Row>
-                        <Row>
-                            <Col span={8}><b>终止日期：</b>王小明      2016-09-26    12:12:12</Col>
-                            <Col span={16}><b>终止原因：</b>终止原因终止原因终止原因终止原因</Col>
-                        </Row>
-                    </div>
-                </div>
                 <div className="wrapbox">
                     <div className="title">
                         租赁费用
