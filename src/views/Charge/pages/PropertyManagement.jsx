d845efea61a6f7f6b7d41304c29948a5fa02cb36
yanxiaodi <929213769@qq.com>
2017-07-17 15:38:51 +0800

    :art: 添加'收费管理-物业费管理' 与 '收费管理-物业费管理(详情); 测试成功;'

---
+++ src/views/Charge/pages/PropertyManagement.jsx
@@ -0,0 +1,12 @@
+// 收费管理 - 物业费管理
+import React from 'react'
+
+class PropertyManagement extends React.Component {
+    render () {
+        return (
+            <h1> 收费管理 - 物业费管理 </h1>
+        )
+    }
+}
+
+export default PropertyManagement
