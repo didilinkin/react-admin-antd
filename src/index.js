304627e7605af469fada085732a325ae29ca8943
yanxiaodi <929213769@qq.com>
2017-07-17 20:40:51 +0800

    :package: install 'store.js';

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 import React from 'react'
 import { render } from 'react-dom'
 
-import { configureStore, history } from './store/configureStore/index' // configureStore
+import { configureStore, history } from './store/configureStore/index'
 import Root from './containers/Root/index'
 
 const store = configureStore()
