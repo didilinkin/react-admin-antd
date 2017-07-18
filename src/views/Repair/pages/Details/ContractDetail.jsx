7c6eb6726e8b8b301076d1a58c03dcf6e0e41907
liuzhiqiang <liuzhiqiang@forevercj.com>
2017-07-18 10:42:54 +0800

    时间

--- src/views/Repair/pages/Details/ContractDetail.jsx
+++ src/views/Repair/pages/Details/ContractDetail.jsx
@@ -19,6 +19,10 @@ class App extends React.Component {
             {'id': this.props.match.params.id,
                 type: 1}
         )
+        this.setState({
+            ListSublet: contract.data.subletInfoList,
+            contract: contract.data.contract
+        })
         console.log(contract.data.contract)
         console.log(contract.data.subletInfoList)
     }
@@ -30,12 +34,12 @@ class App extends React.Component {
             <div className="contract">
                 <h2>房源信息</h2>
                 <Row>
-                    <Col span={8}><b>所属楼宇：</b>长江中心A座 </Col>
-                    <Col span={8}><b>服务面积：</b>d</Col>
-                    <Col span={8}><b>房间别名：</b>123</Col>
+                    <Col span={8}><b>所属楼宇：</b>{this.state.contract.buildName} </Col>
+                    <Col span={8}><b>服务面积：</b>{this.state.contract.serviceArea}</Col>
+                    <Col span={8} />
                 </Row>
                 <Row>
-                    <Col span={24}><b>房间编号：</b>2301/2302/2303/2305/2306 </Col>
+                    <Col span={24}><b>房间编号：</b>{this.state.contract.leaseRooms} </Col>
                 </Row>
                 <div className="wrapbox">
                     <div className="title">
@@ -58,29 +62,32 @@ class App extends React.Component {
                             <Col span={8}><b>公司编号：</b>2017-7-11 14:17:07 </Col>
                             <Col span={8} />
                         </Row>
-                        <p className="line" />
-                        <h3>转租信息 <span className="green">编辑</span> <span className="red">删除</span></h3>
-                        <Row>
-                            <Col span={8}><b>租户名称：</b>长江中心A座 </Col>
-                            <Col span={8}><b>联系人：</b>对对对</Col>
-                            <Col span={8}><b>行政电话：</b>123456789</Col>
-                        </Row>
-                        <Row>
-                            <Col span={8}><b>财务电话：</b>123456789 </Col>
-                            <Col span={8}><b>经理电话：</b>123456789</Col>
-                            <Col span={8}><b>E-mail：</b>123456789@163.com</Col>
-                        </Row>
-                        <Row>
-                            <Col span={8}><b>转租周期：</b>2017-7-11 14:17:07 </Col>
-                            <Col span={16}><b>转租房间：</b>2301/2302/2303/2305/2306 </Col>
-                        </Row>
-                        <Row>
-                            <Col span={8}><b>公司编号：</b>2017-7-11 14:17:07 </Col>
-                            <Col span={8} />
-                            <Col span={8} />
-                        </Row>
+                        {this.state.ListSublet.map((sublet, i) => {
+                            return <div key={i}>
+                                <p className="line" />
+                                <h3>转租信息 <span className="green">编辑</span> <span className="red">删除</span></h3>
+                                <Row>
+                                    <Col span={8}><b>租户名称：</b>{sublet.tenant} </Col>
+                                    <Col span={8}><b>联系人：</b>{sublet.contactPerson}</Col>
+                                    <Col span={8}><b>行政电话：</b>{sublet.phoneAdmin}</Col>
+                                </Row>
+                                <Row>
+                                    <Col span={8}><b>财务电话：</b>{sublet.phoneFinance} </Col>
+                                    <Col span={8}><b>经理电话：</b>{sublet.phoneManager}</Col>
+                                    <Col span={8}><b>E-mail：</b>{sublet.email}</Col>
+                                </Row>
+                                <Row>
+                                    <Col span={8}><b>转租周期：</b>{sublet.subletStartDate} -- {sublet.subletEndDate} </Col>
+                                    <Col span={16}><b>转租房间：</b>{sublet.roomNum} </Col>
+                                </Row>
+                                <Row>
+                                    <Col span={8}><b>公司编号：</b>2017-7-11 14:17:07 </Col>
+                                    <Col span={8} />
+                                    <Col span={8} />
+                                </Row>
+                            </div>
+                        })}
                     </div>
-
                 </div>
                 <div className="wrapbox">
                     <div className="title">
