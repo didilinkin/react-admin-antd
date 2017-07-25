a70e7c7da04d7a56c2c7425dc4d93e60d954f998
yanxiaodi <929213769@qq.com>
2017-07-25 17:31:39 +0800

    :bug: 匹配页面(内容), 接收route参数, 然后可以得到 更深一层的route对象; 官网案例不适用此处; 需要修改routes 为 route;

--- src/router/basicRoutes.js
+++ src/router/basicRoutes.js
@@ -1,7 +1,7 @@
 // 基础常用 路由配置表(404 / Login 等页面)
 import React from 'react'
 
-const NoMatch = () => <h3>404 页面</h3>
+const NoMatch = () => <h3>404 页面: 红牛</h3>
 
 const basicRoutes = [
     {
--- src/router/matchingRoutes.js
+++ src/router/matchingRoutes.js
@@ -1,14 +1,63 @@
 // 映射 路由配置表(项目逻辑)
+import React from 'react'
+
+// import { Route } from 'react-router-dom'
 
 // 将 globalDir 中的映射处理好
 // import globalDir from '../utils/globalDir'
 
 import Sider from '../common/containers/Sider'
 
+// 测试'路由配置'
+const Snacks = ({ route }) => (
+    <div>
+        <h2>小吃</h2>
+        <ul>
+            <li>辣条</li>
+            <li>薯片</li>
+        </ul>
+
+        <hr />
+        {
+            console.log(route)
+        }
+        {/*
+            {
+                routes.map((route, i) => (
+                    <RouteWithSubRoutes key={ i } { ...route} />
+                ))
+            }
+        */}
+    </div>
+)
+
+// const RouteWithSubRoutes = (route) => (
+//     <Route path={ route.path } render={ props => (
+//         // 把自路由向下传递来达到嵌套。
+//         <route.component { ...props } routes={ route.routes } />
+//     )}
+//     />
+// )
+
+const Spicy = () => <h3>辣条</h3>
+const Chips = () => <h3>薯片</h3>
+
 const matchingRoutes = [
     {
         path: '/sider',
         component: Sider
+    }, {
+        path: '/snacks',
+        component: Snacks,
+        routes: [
+            {
+                path: '/snacks/spicy',
+                component: Spicy
+            }, {
+                path: '/snacks/chips',
+                component: Chips
+            }
+        ]
     }
 ]
 
