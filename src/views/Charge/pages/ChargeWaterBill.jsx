fdc9a1429a86371ef91f2b86b7423fb0777e0f90
liuzhiqiang <liuzhiqiang@forevercj.com>
2017-08-01 16:48:36 +0800

    合同 转租 终止

--- src/views/Charge/pages/ChargeWaterBill.jsx
+++ src/views/Charge/pages/ChargeWaterBill.jsx
@@ -1,10 +1,437 @@
 // 物业管理 - 水费审核
 import React from 'react'
-
+import {Table, Spin, Popconfirm, Tabs  } from 'antd'
+import WaterBillHeadComponent from './components/WaterBillHead'
+import { apiPost } from '../../../api'
+const TabPane = Tabs.TabPane
 class ChargeWaterBill extends React.Component {
+    constructor (props) {
+        super(props)
+        this.state = {
+            loading: false,
+            columns1: [],
+            columns2: [],
+            columns3: [],
+            columns4: [],
+            dataSource1: [],
+            dataSource2: [],
+            dataSource3: [],
+            dataSource4: [],
+            ListBuildingInfo: [],
+            RowKeys: [],
+            id: 0
+        }
+    }
+    refresh = async (pagination, filters, sorter) => {
+        this.setState({loading: true})
+        let result = await apiPost(
+            '/WaterBill/WaterBillList',
+            filters
+        )
+        let WaterBillList = result.data
+        let dataSource1 = []
+        let dataSource2 = []
+        let dataSource3 = []
+        let dataSource4 = []
+        WaterBillList.map(WaterBill => {
+            if (WaterBill.examineState.toString() === '0') {
+                dataSource1.push(WaterBill)
+            } else if (WaterBill.examineState.toString() === '1') {
+                dataSource2.push(WaterBill)
+            } else if (WaterBill.examineState.toString() === '2') {
+                dataSource4.push(WaterBill)
+            } else if (WaterBill.examineState.toString() === '3') {
+                dataSource3.push(WaterBill)
+            }
+            return ''
+        })
+        this.setState({
+            loading: false,
+            dataSource1: dataSource1,
+            dataSource2: dataSource2,
+            dataSource3: dataSource3,
+            dataSource4: dataSource4
+        })
+    }
+    async initialRemarks () {
+        this.setState({loading: true})
+        let result = await apiPost(
+            '/WaterBill/WaterBillList',
+        )
+        let ListBuildingInfo = await apiPost(
+            '/collectRent/ListBuildingInfo',
+        )
+        let WaterBillList = result.data
+        let dataSource1 = []
+        let dataSource2 = []
+        let dataSource3 = []
+        let dataSource4 = []
+        WaterBillList.map(WaterBill => {
+            if (WaterBill.examineState.toString() === '0') {
+                dataSource1.push(WaterBill)
+            } else if (WaterBill.examineState.toString() === '1') {
+                dataSource2.push(WaterBill)
+            } else if (WaterBill.examineState.toString() === '2') {
+                dataSource4.push(WaterBill)
+            } else if (WaterBill.examineState.toString() === '3') {
+                dataSource3.push(WaterBill)
+            }
+            return ''
+        })
+        this.setState({
+            ListBuildingInfo: ListBuildingInfo.data,
+            loading: false,
+            dataSource1: dataSource1,
+            dataSource2: dataSource2,
+            dataSource3: dataSource3,
+            dataSource4: dataSource4,
+            columns1: [{
+                title: '序号',
+                width: 100,
+                dataIndex: 'id',
+                render: function (text, record, index) {
+                    index++
+                    return (
+                        <span>{index}</span>
+                    )
+                }
+            }, {
+                title: '所属楼宇',
+                width: 100,
+                dataIndex: 'buildName'
+            }, {
+                title: '房间编号',
+                width: 100,
+                dataIndex: 'roomNumber'
+            }, {
+                title: '客户名称',
+                width: 100,
+                dataIndex: 'clientSubletName'
+            }, {
+                title: '本期水费周期',
+                width: 200,
+                dataIndex: 'cycle'
+            }, {
+                title: '本次总水量',
+                width: 100,
+                dataIndex: 'totalWater'
+            }, {
+                title: ' 本次应收',
+                width: 100,
+                dataIndex: 'receivableMoney'
+            }, {
+                title: ' 交费期限',
+                width: 100,
+                dataIndex: 'overdueDate'
+            }, {
+                title: ' 操作',
+                width: 200,
+                dataIndex: 'opt',
+                render: function (text, record, index) {
+                    return (
+                        <span>
+                            <a>明细</a>
+                            &nbsp;&nbsp;&nbsp;&nbsp;
+                            <a>修改</a>
+                            &nbsp;&nbsp;&nbsp;&nbsp;
+                            <Popconfirm key="1" title="确定提交吗?">
+                                <a>提交</a>
+                            </Popconfirm>
+                        </span>
+                    )
+                }
+            }],
+            columns2: [{
+                title: '序号',
+                width: 100,
+                dataIndex: 'id',
+                render: function (text, record, index) {
+                    index++
+                    return (
+                        <span>{index}</span>
+                    )
+                }
+            }, {
+                title: '所属楼宇',
+                width: 100,
+                dataIndex: 'buildName'
+            }, {
+                title: '房间编号',
+                width: 100,
+                dataIndex: 'roomNumber'
+            }, {
+                title: '客户名称',
+                width: 100,
+                dataIndex: 'clientSubletName'
+            }, {
+                title: '本期水费周期',
+                width: 200,
+                dataIndex: 'cycle'
+            }, {
+                title: '本次总水量',
+                width: 100,
+                dataIndex: 'totalWater'
+            }, {
+                title: ' 本次应收',
+                width: 100,
+                dataIndex: 'receivableMoney'
+            }, {
+                title: ' 交费期限',
+                width: 100,
+                dataIndex: 'overdueDate'
+            }, {
+                title: ' 操作',
+                width: 200,
+                dataIndex: 'opt',
+                render: function (text, record, index) {
+                    return (
+                        <span>
+                            <a>明细</a>
+                        </span>
+                    )
+                }
+            }],
+            columns3: [{
+                title: '序号',
+                width: 100,
+                dataIndex: 'id',
+                render: function (text, record, index) {
+                    index++
+                    return (
+                        <span>{index}</span>
+                    )
+                }
+            }, {
+                title: '所属楼宇',
+                width: 100,
+                dataIndex: 'buildName'
+            }, {
+                title: '房间编号',
+                width: 100,
+                dataIndex: 'roomNumber'
+            }, {
+                title: '客户名称',
+                width: 100,
+                dataIndex: 'clientSubletName'
+            }, {
+                title: '本期水费周期',
+                width: 200,
+                dataIndex: 'cycle'
+            }, {
+                title: '本次总水量',
+                width: 100,
+                dataIndex: 'totalWater'
+            }, {
+                title: '本次应收',
+                width: 100,
+                dataIndex: 'receivableMoney'
+            }, {
+                title: '交费期限',
+                width: 100,
+                dataIndex: 'overdueDate'
+            }, {
+                title: '审核说明',
+                width: 100,
+                dataIndex: 'auditExplain'
+            }, {
+                title: '审核时间',
+                width: 100,
+                dataIndex: 'auditDate'
+            }, {
+                title: '审核人',
+                width: 100,
+                dataIndex: 'auditName'
+            }, {
+                title: ' 操作',
+                width: 200,
+                dataIndex: 'opt',
+                render: function (text, record, index) {
+                    return (
+                        <span>
+                            <Popconfirm key="1" title="确定重新收费吗?">
+                                <a>重新收费</a>
+                            </Popconfirm>
+                            &nbsp;&nbsp;&nbsp;&nbsp;
+                            <a>明细</a>
+                        </span>
+                    )
+                }
+            }],
+            columns4: [{
+                title: '序号',
+                width: 100,
+                dataIndex: 'id',
+                render: function (text, record, index) {
+                    index++
+                    return (
+                        <span>{index}</span>
+                    )
+                }
+            }, {
+                title: '所属楼宇',
+                width: 100,
+                dataIndex: 'buildName'
+            }, {
+                title: '房间编号',
+                width: 100,
+                dataIndex: 'roomNumber'
+            }, {
+                title: '客户名称',
+                width: 100,
+                dataIndex: 'clientSubletName'
+            }, {
+                title: '本期水费周期',
+                width: 200,
+                dataIndex: 'cycle'
+            }, {
+                title: '本次总水量',
+                width: 100,
+                dataIndex: 'totalWater'
+            }, {
+                title: '本次应收',
+                width: 100,
+                dataIndex: 'receivableMoney'
+            }, {
+                title: '交费期限',
+                width: 100,
+                dataIndex: 'overdueDate'
+            }, {
+                title: '实交日期',
+                width: 100,
+                dataIndex: 'collectionDate'
+            }, {
+                title: '逾期天数',
+                width: 100,
+                dataIndex: 'days'
+            }, {
+                title: '延期下月电费',
+                width: 100,
+                dataIndex: 'penaltyType',
+                render: function (text, record, index) {
+                    let penaltyType = '否'
+                    if (text.toString() === '1') {
+                        penaltyType = '是'
+                    }
+                    return (
+                        <p>{penaltyType}</p>
+                    )
+                }
+            }, {
+                title: '开票状态',
+                width: 100,
+                dataIndex: 'billingState',
+                render: function (text, record, index) {
+                    let billingState = '未开票'
+                    if (text.toString() === '1') {
+                        billingState = '已开票'
+                    }
+                    return (
+                        <p>{billingState}</p>
+                    )
+                }
+            }, {
+                title: ' 操作',
+                width: 200,
+                dataIndex: 'opt',
+                fixed: 'right',
+                render: function (text, record, index) {
+                    return (
+                        <span>
+                            <a>明细</a>
+                            &nbsp;&nbsp;&nbsp;&nbsp;
+                            <Popconfirm key="1" title="确定提交吗?">
+                                <a>打印通知单</a>
+                            </Popconfirm>
+                        </span>
+                    )
+                }
+            }]
+        })
+    }
+    componentDidMount () {
+        this.initialRemarks()
+    }
+    onSelectChange = (selectedRowKeys) => {
+        console.log('selectedRowKeys changed: ', selectedRowKeys)
+        this.setState({
+            RowKeys: selectedRowKeys
+        })
+    }
     render () {
         return (
-            <h1> 物业管理 - 水费审核 </h1>
+            <Spin spinning={this.state.loading}>
+                <Tabs defaultActiveKey="1">
+                    <TabPane tab="待发起" key="1">
+                        <WaterBillHeadComponent
+                            RowKeys={this.state.RowKeys}
+                            refresh={this.refresh}
+                            type={1}
+                            ListBuildingInfo={this.state.ListBuildingInfo}
+                        />
+                        <Table
+                        // pagination = <Pagination showSizeChanger total={500}/>
+                            rowSelection={{
+                                onChange: this.onSelectChange
+                            }}
+                            onChange={this.refresh}
+                            dataSource={this.state.dataSource1}
+                            columns={this.state.columns1}
+                        />
+                    </TabPane>
+                    <TabPane tab="审核中" key="2">
+                        <WaterBillHeadComponent
+                            RowKeys={this.state.RowKeys}
+                            refresh={this.refresh}
+                            type={2}
+                            ListBuildingInfo={this.state.ListBuildingInfo}
+                        />
+                        <Table
+                            // pagination = <Pagination showSizeChanger total={500}/>
+                            rowSelection={{
+                                onChange: this.onSelectChange
+                            }}
+                            onChange={this.refresh}
+                            dataSource={this.state.dataSource2}
+                            columns={this.state.columns2}
+                        />
+                    </TabPane>
+                    <TabPane tab="审核失败" key="3">
+                        <WaterBillHeadComponent
+                            RowKeys={this.state.RowKeys}
+                            refresh={this.refresh}
+                            type={3}
+                            ListBuildingInfo={this.state.ListBuildingInfo}
+                        />
+                        <Table
+                            // pagination = <Pagination showSizeChanger total={500}/>
+                            rowSelection={{
+                                onChange: this.onSelectChange
+                            }}
+                            onChange={this.refresh}
+                            dataSource={this.state.dataSource3}
+                            columns={this.state.columns3}
+                        />
+                    </TabPane>
+                    <TabPane tab="审核成功" key="4">
+                        <WaterBillHeadComponent
+                            RowKeys={this.state.RowKeys}
+                            refresh={this.refresh}
+                            type={4}
+                            ListBuildingInfo={this.state.ListBuildingInfo}
+                        />
+                        <Table
+                            // pagination = <Pagination showSizeChanger total={500}/>
+                            rowSelection={{
+                                onChange: this.onSelectChange
+                            }}
+                            onChange={this.refresh}
+                            scroll={{ x: 1450 }}
+                            dataSource={this.state.dataSource4}
+                            columns={this.state.columns4}
+                        />
+                    </TabPane>
+                </Tabs>
+            </Spin>
         )
     }
 }
