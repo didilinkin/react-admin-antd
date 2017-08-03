e27f885e8258e01e281b599d0a6efb115c30c1c3
alanyangcn <215410815@qq.com>
2017-08-03 09:09:21 +0800

    no message

---
+++ src/views/Charge/pages/components/PowerAddUp.jsx
@@ -0,0 +1,126 @@
+import React from 'react'
+import {Form, Row, Col, Input, Button, DatePicker} from 'antd'
+import PowerMeterWrite from './PowerMeterWrite'
+const FormItem = Form.Item
+const {RangePicker} = DatePicker
+class PowerAddUp extends React.Component {
+    state = {
+        visible: false,
+        isFirst: true,
+        PmContract: [],
+        WaterRecordlList: []
+    }
+    render () {
+        const { getFieldDecorator } = this.props.form
+        return (
+            <Form layout="horizontal">
+                <Row>
+                    <Col span={8}>
+                        <FormItem label="本次周期" labelCol={{ span: 6 }}
+                            wrapperCol={{ span: 15 }}
+                        >
+                            {getFieldDecorator('sfzq', {
+                                rules: [ {
+                                    required: true,
+                                    message: '请选择本次周期!'
+                                }]
+                            })(<RangePicker />)}
+                        </FormItem>
+                    </Col>
+                    <Col span={8}>
+                        <FormItem label="交费期限" labelCol={{ span: 6 }}
+                            wrapperCol={{ span: 15 }}
+                        >
+                            {getFieldDecorator('overdueDate', {
+                                rules: [ {
+                                    required: true,
+                                    message: '请填写交费期限!'
+                                }]
+                            })(<DatePicker />)}
+                        </FormItem>
+                    </Col>
+                    <Col span={8}>
+                        <FormItem label="抄表人员" labelCol={{ span: 6 }}
+                            wrapperCol={{ span: 15 }}
+                        >
+                            {getFieldDecorator('readId', {
+                                rules: [ {
+                                    required: true,
+                                    message: '请填写抄表人员!'
+                                }]
+                            })(<Input />)}
+                        </FormItem>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col span={8}>
+                        <FormItem label="客户名称" labelCol={{ span: 6 }}
+                            wrapperCol={{ span: 15 }}
+                        >
+                            {getFieldDecorator('voucherNo')(<Input />)}
+                        </FormItem>
+                    </Col>
+                    <Col span={8}>
+                        <FormItem label="转租客户" labelCol={{ span: 6 }}
+                            wrapperCol={{ span: 15 }}
+                        >
+                            {getFieldDecorator('acceptor')(<Input />)}
+                        </FormItem>
+                    </Col>
+                    <Col span={8}>
+                        <FormItem label="房间编号" labelCol={{ span: 6 }}
+                            wrapperCol={{ span: 15 }}
+                        >
+                            {getFieldDecorator('rrr')(<Input />)}
+                        </FormItem>
+                    </Col>
+                </Row>
+                <span style={{textAlign: 'center',
+                    display: 'block'}}
+                >
+                    {getFieldDecorator('eee')(<Input style={{width: '300px'}} />)}
+                    <span>电量统计表</span>
+                </span>
+                <br />
+                <div style={{width: 900,
+                    marginBottom: 20}}
+                >
+                    <table className="tb">
+                        <tbody>
+                            <tr className="hd">
+                                <td>电费名称</td>
+                                <td>上次抄表数</td>
+                                <td>本次抄表数</td>
+                                <td>本次用电量</td>
+                                <td>变比</td>
+                                <td>电损0%</td>
+                                <td>总电量</td>
+                                <td>峰谷比例</td>
+                                <td>单价(1.0685)</td>
+                                <td>金额</td>
+                                <td>备注</td>
+                                <td>操作</td>
+                            </tr>
+                            {this.state.WaterRecordlList.map((WaterRecordl, i) => <tr key={i}>
+                                <td>{WaterRecordl.whTypeName}</td>
+                                <td>{WaterRecordl.storagePlace}</td>
+                                <td>{WaterRecordl.name}</td>
+                                <td>{WaterRecordl.standard}</td>
+                                <td>{WaterRecordl.unit}</td>
+                                <td>{WaterRecordl.unitPrice}</td>
+                                <td>{WaterRecordl.number}</td>
+                                <td>{WaterRecordl.amount}</td>
+                                <td>{WaterRecordl.remark}</td>
+                                <td><Button onClick={() => this.delect(WaterRecordl.uuid)}>删除</Button></td></tr>)}
+                        </tbody>
+                    </table>
+                </div>
+                <PowerMeterWrite />
+                <Button type="primary" onClick={this.add}>提交</Button>
+            </Form>
+        )
+    }
+}
+
+let PowerAddUpComponent = Form.create()(PowerAddUp)
+export default PowerAddUpComponent
---
+++ src/views/Charge/pages/components/PowerMeterWrite.jsx
@@ -0,0 +1,119 @@
+import React from 'react'
+import {Form, Select, Input, Button  } from 'antd'
+
+const FormItem = Form.Item
+const Option = Select.Option
+const {TextArea} = Input
+class PowerMeterWrite extends React.Component {
+    handleSubmit = (e) => {
+        e.preventDefault()
+        this.props.form.validateFieldsAndScroll((err, values) => {
+            if (!err) {
+                console.log('Received values of form: ', values)
+                alert('dad')
+            }
+        })
+    }
+    render () {
+        const { getFieldDecorator } = this.props.form
+        const formItemLayout = {
+            labelCol: {
+                xs: { span: 24 },
+                sm: { span: 6 }
+            },
+            wrapperCol: {
+                xs: { span: 24 },
+                sm: { span: 14 }
+            }
+        }
+        const tailFormItemLayout = {
+            wrapperCol: {
+                xs: {
+                    span: 24,
+                    offset: 0
+                },
+                sm: {
+                    span: 14,
+                    offset: 6
+                }
+            }
+        }
+
+        const titleLayout = {
+            color: '#ffffff',
+            height: 50,
+            backgroundColor: '#199BFC',
+            textAlign: 'center',
+            fontSize: 20
+        }
+
+
+        return (
+            <div style={{ width: 300,
+                border: '1px solid #999'
+            }}
+            >
+                <div style={titleLayout}>抄表录入</div>
+                <Form onSubmit={this.handleSubmit}>
+                    <FormItem
+                        {...formItemLayout}
+                        label="房间编号"
+                    >{getFieldDecorator('room')(<Select>
+                            <Option value="0001">0001</Option>
+                            <Option value="0002">0002</Option>
+                            <Option value="0003">0003</Option>
+                        </Select>)
+                        }
+                    </FormItem>
+                    <FormItem
+                        {...formItemLayout}
+                        label="费用名称"
+                    >{getFieldDecorator('name')(<Input placeholder="请输入内容" />)
+                        }
+                    </FormItem>
+                    <FormItem
+                        {...formItemLayout}
+                        label="上次抄表数"
+                    >{getFieldDecorator('lastMeterNumber')(<Input placeholder="请输入内容" />)
+                        }
+                    </FormItem>
+                    <FormItem
+                        {...formItemLayout}
+                        label="本次抄表数"
+                    >{getFieldDecorator('meterNumber')(<Input placeholder="请输入内容" />)
+                        }
+                    </FormItem>
+                    <FormItem
+                        {...formItemLayout}
+                        label="本次用电量"
+                    >{getFieldDecorator('useNumber')(<Input placeholder="请输入内容" />)
+                        }
+                    </FormItem>
+                    <FormItem
+                        {...formItemLayout}
+                        label="变比"
+                    >{getFieldDecorator('chenge')(<Input placeholder="请输入内容" />)
+                        }
+                    </FormItem>
+                    <FormItem
+                        {...formItemLayout}
+                        label="单价"
+                    >{getFieldDecorator('price')(<Input placeholder="请输入内容" />)
+                        }
+                    </FormItem>
+                    <FormItem
+                        {...formItemLayout}
+                        label="备注"
+                    >{getFieldDecorator('note')(<TextArea rows={10} />)
+                        }
+                    </FormItem>
+                    <FormItem {...tailFormItemLayout}>
+                        <Button type="primary" htmlType="submit">增加本条记录</Button>
+                    </FormItem>
+                </Form>
+            </div>)
+    }
+}
+
+const PowerMeterWriteComponent = Form.create()(PowerMeterWrite)
+export default PowerMeterWriteComponent
