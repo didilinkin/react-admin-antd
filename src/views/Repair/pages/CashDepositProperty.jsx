253cf82f54e0457a86d0442ba63635446cd90b8e
liuzhiqiang <liuzhiqiang@forevercj.com>
2017-08-08 10:24:02 +0800

    合同 转租 终止

---
+++ src/views/Repair/pages/CashDepositProperty.jsx
@@ -0,0 +1,172 @@
+// 能源管理押金
+import React, {Component} from 'react'
+import {Table, Button, Spin, Input, Select } from 'antd'
+import { apiPost } from '../../../api'
+// 引入组件
+const Option = Select.Option
+// React component
+class CashDepositProperty extends Component {
+    constructor (props) {
+        super(props)
+        this.state = {
+            loading: false,
+            openAdd: false,
+            openTableAddUp: false,
+            openUpdate: false,
+            columns: [],
+            dataSource: [],
+            ListBuildingInfo: []
+        }
+    }
+    handleUpdate = (id) => {
+        this.setState({
+            openAdd: false,
+            openTableAddUp: false,
+            openUpdate: true,
+            id: id
+        })
+    }
+    async initialRemarks () {
+        this.setState({loading: true})
+        let result = await apiPost(
+            '/cashDeposit/cashDepositList',
+            {chargeItem: 2}
+        )
+        let ListBuildingInfo = await apiPost(
+            '/collectRent/ListBuildingInfo'
+        )
+        const handleUpdate = this.handleUpdate
+        this.setState({loading: false,
+            ListBuildingInfo: ListBuildingInfo.data,
+            columns: [{
+                title: '序号',
+                width: 100,
+                dataIndex: 'id',
+                key: 'id',
+                render: function (text, record, index) {
+                    index++
+                    return (
+                        <span>{index}</span>
+                    )
+                }
+            }, {
+                title: '所属楼宇',
+                width: 150,
+                dataIndex: 'buildName',
+                key: 'buildName'
+            }, {
+                title: '房间编号',
+                width: 250,
+                dataIndex: 'roomNum',
+                key: 'roomNum'
+            }, {
+                title: '客户名称',
+                width: 300,
+                dataIndex: 'sublietName',
+                key: 'sublietName'
+            }, {
+                title: '当前结余',
+                width: 250,
+                dataIndex: 'currentBalance',
+                key: 'currentBalance'
+            }, {
+                title: '操作',
+                width: 200,
+                dataIndex: 'opt',
+                key: 'opt',
+                fixed: 'right',
+                render: function (text, record, index) {
+                    return (
+                        <div>
+                            <a href="javascript:" onClick={() => handleUpdate(record.id)} > 审核 </a>
+                        </div>
+                    )
+                }
+            }],
+            dataSource: result.data
+        })
+    }
+    componentDidMount () {
+        this.initialRemarks()
+    }
+    refresh = async () => {
+        // 刷新表格
+        let result = await apiPost(
+            '/cashDeposit/cashDepositList',
+            {'sublietName': this.sublietName,
+                'roomNum': this.roomNum,
+                'buildId': this.buildId,
+                'chargeItem': 0
+            }
+        )
+        this.setState({
+            openAdd: false,
+            openTableAddUp: false,
+            openUpdate: false,
+            dataSource: result.data,
+            id: 0
+        })
+    }
+    sublietName = null
+    entryNameOnChange = (e) => {
+        this.sublietName = e.target.value
+    }
+    roomNum = null
+    entryNumberOnChange = (e) => {
+        this.roomNum = e.target.value
+    }
+    buildId = null
+    selectBuild = (e) => {
+        this.buildId = e
+    }
+    query = () => {
+        this.refresh()
+    }
+    render () {
+        let ListBuildingInfo = this.state.ListBuildingInfo
+        return (
+            <div>
+                <span style={{paddingBottom: '10px',
+                    paddingTop: '10px',
+                    display: 'block'}}
+                >
+                    <span>所属楼宇:&nbsp;&nbsp;</span>
+                    <Select
+                        showSearch
+                        allowClear
+                        style={{width: 200,
+                            marginRight: '5px'}}
+                        placeholder="请选择所属楼宇"
+                        optionFilterProp="children"
+                        onChange={this.selectBuild}
+                    >
+                        {ListBuildingInfo.map(BuildingInfo => {
+                            return <Option key={BuildingInfo.id}>{BuildingInfo.buildName}</Option>
+                        })}
+                    </Select>
+                    <span>房间编号:&nbsp;&nbsp;</span>
+                    <Input style={{width: 150,
+                        marginRight: '5px'}} onChange={this.entryNumberOnChange}
+                    />
+                    <span>&nbsp;&nbsp;&nbsp;&nbsp;客户名称:&nbsp;&nbsp;</span>
+                    <Input style={{width: 150,
+                        marginRight: '5px'}} onChange={this.entryNameOnChange}
+                    />
+                    <Button type="primary" onClick={this.query}>查询</Button>
+                </span>
+
+                <Spin spinning={this.state.loading}>
+                    <Table
+                        scroll={{ x: 1200 }}
+                        bordered
+                        dataSource={this.state.dataSource}
+                        columns={this.state.columns}
+                    />
+                </Spin>
+            </div>
+        )
+    }
+}
+export default CashDepositProperty
+
+
