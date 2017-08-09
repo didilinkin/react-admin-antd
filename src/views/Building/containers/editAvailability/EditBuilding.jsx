1cb8e80fbb809dddac7428cdf6a1c43ab39f56f2
damon <limeng@forevercj.com>
2017-08-09 13:41:19 +0800

    楼宇完成

--- src/views/Building/containers/editAvailability/EditBuilding.jsx
+++ src/views/Building/containers/editAvailability/EditBuilding.jsx
@@ -1,14 +1,156 @@
-// 楼宇管理 - 编辑房源 - 编辑楼宇
-import React from 'react'
-
-class EditBuilding extends React.Component {
+// 楼宇列表
+import React, {Component} from 'react'
+import {Table, Button, Spin, Input } from 'antd'
+import { apiPost } from '../../../../api'
+import AddBuilding from './BuidAdd'
+// 引入组件
+// React component
+class EditBuilding extends Component {
+    constructor (props) {
+        super(props)
+        this.state = {
+            loading: false,
+            openAdd: false,
+            openTableAddUp: false,
+            openUpdate: false,
+            columns: [],
+            dataSource: [],
+            title: '',
+            id: null,
+            ListBuildingInfo: []
+        }
+    }
+    handleUpdate = (id) => {
+        this.setState({
+            openAdd: true,
+            openTableAddUp: false,
+            openUpdate: false,
+            title: '修改楼宇',
+            id: id
+        })
+    }
+    add = () => {
+        this.setState({
+            openAdd: true,
+            openTableAddUp: false,
+            openUpdate: false,
+            title: '添加楼宇',
+            id: null
+        })
+    }
+    async initialRemarks () {
+        this.setState({loading: true})
+        let result = await apiPost(
+            '/build/buildList'
+        )
+        const handleUpdate = this.handleUpdate
+        this.setState({loading: false,
+            columns: [{
+                title: '序号',
+                width: 100,
+                dataIndex: 'id',
+                key: 'id',
+                render: function (text, record, index) {
+                    index++
+                    return (
+                        <span>{index}</span>
+                    )
+                }
+            }, {
+                title: '楼宇名称',
+                width: 150,
+                dataIndex: 'buildName',
+                key: 'buildName'
+            }, {
+                title: '楼层数量',
+                width: 250,
+                dataIndex: 'floorNum',
+                key: 'floorNum'
+            }, {
+                title: '客梯数量',
+                width: 300,
+                dataIndex: 'passengerElevatorNum',
+                key: 'passengerElevatorNum'
+            }, {
+                title: '货梯数量',
+                width: 250,
+                dataIndex: 'goodsElevatorNum',
+                key: 'goodsElevatorNum'
+            }, {
+                title: '操作',
+                width: 200,
+                dataIndex: 'opt',
+                key: 'opt',
+                fixed: 'right',
+                render: function (text, record, index) {
+                    return (
+                        <div>
+                            <a href="javascript:" onClick={() => handleUpdate(record.id)} > 编辑 </a>
+                        </div>
+                    )
+                }
+            }],
+            dataSource: result.data
+        })
+    }
+    componentDidMount () {
+        this.initialRemarks()
+    }
+    refresh = async () => {
+        // 刷新表格
+        let result = await apiPost(
+            '/build/buildList',
+            {'buildName': this.buildName
+            }
+        )
+        this.setState({
+            openAdd: false,
+            openTableAddUp: false,
+            openUpdate: false,
+            dataSource: result.data,
+            id: 0
+        })
+    }
+    buildName = null
+    entryNameOnChange = (e) => {
+        this.buildName = e.target.value
+    }
+    query = () => {
+        this.refresh()
+    }
     render () {
         return (
             <div>
-                <h1> 楼宇管理 - 编辑房源 - 编辑楼宇 </h1>
+                <AddBuilding
+                    id={this.state.id}
+                    refreshTable={this.refresh}
+                    visible={this.state.openAdd}
+                    title={this.state.title}
+                />
+                <span style={{paddingBottom: '10px',
+                    paddingTop: '10px',
+                    display: 'block'}}
+                >
+                    <span>&nbsp;&nbsp;&nbsp;&nbsp;楼宇名称:&nbsp;&nbsp;</span>
+                    <Input style={{width: 150,
+                        marginRight: '5px'}} onChange={this.entryNameOnChange}
+                    />
+                    <Button type="primary" onClick={this.query}>查询</Button>
+                    <Button type="primary" onClick={this.add}>添加楼宇</Button>
+                </span>
+
+                <Spin spinning={this.state.loading}>
+                    <Table
+                        scroll={{ x: 1100 }}
+                        bordered
+                        dataSource={this.state.dataSource}
+                        columns={this.state.columns}
+                    />
+                </Spin>
             </div>
         )
     }
 }
-
 export default EditBuilding
+
+
