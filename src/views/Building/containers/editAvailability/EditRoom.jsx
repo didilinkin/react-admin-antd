1b48f6815d5a21be7656bc519774076a58b10712
yanxiaodi <929213769@qq.com>
2017-08-09 10:36:57 +0800

    :heavy_plus_sign: 添加新版块'楼宇管理'; 配置路由与左侧导航; 测试完成;

---
+++ src/views/Building/containers/editAvailability/EditRoom.jsx
@@ -0,0 +1,14 @@
+// 楼宇管理 - 编辑房源 - 编辑房间
+import React from 'react'
+
+class EditRoom extends React.Component {
+    render () {
+        return (
+            <div>
+                <h1> 楼宇管理 - 编辑房源 - 编辑房间 </h1>
+            </div>
+        )
+    }
+}
+
+export default EditRoom
