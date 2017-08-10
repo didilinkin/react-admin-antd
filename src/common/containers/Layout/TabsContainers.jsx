93a1726ea9a86bca746eaa50694b7c6dfa96fc93
yanxiaodi <929213769@qq.com>
2017-08-10 13:38:37 +0800

    :fire: 删除部分注释; 测试成功;

--- src/common/containers/Layout/TabsContainers.jsx
+++ src/common/containers/Layout/TabsContainers.jsx
@@ -22,11 +22,6 @@ class TabsContainers extends React.Component {
 
     // 切换面板的回调 => 切换 state.activeKey
     onChange = (activeKey) => {
-        // 删除 非最后一个标签时, 跳转事件存在问题
-        console.log('activeKey值:' + activeKey)
-        console.log('切换标签, 检查panesState数据')
-        console.dir(this.state.panes)
-
         let currentKey
         let currentIndex
         let currentUrl
@@ -36,19 +31,9 @@ class TabsContainers extends React.Component {
                 currentIndex = i
             }
         })
-
-
-        console.log(currentIndex)
-
         currentKey = this.state.panes[currentIndex].key
         currentUrl = this.state.panes[currentIndex].path
-
-        console.log('currentKey:' + currentKey)
-
-        console.log(typeof currentKey)
-
         this.setActiveKey(currentKey) // 改变 activeKey
-
         this.props.tabsProps.history.push(currentUrl)
     }
 
@@ -65,20 +50,16 @@ class TabsContainers extends React.Component {
         const hasUrl = hasString(arrayPanes, 'path', strUrl)
 
         if (!isHomeIndex) {
-            if (hasUrl < 1) {
-                // console.log('无 当前url')
+            if (hasUrl < 1) { // console.log('无 当前url')
                 let currentPanes = this.setCloneObj() // 单个
                 this.setActions(`${arrayPanes.length + 1}`, currentPanes) // 加入 store
-            } else {
-                // console.log('有 当前url')
+            } else { // console.log('有 当前url')
                 let currentKey
-
                 arrayPanes.forEach((pane, i) => {
                     if (pane.path === strUrl) {
                         currentKey = pane.key
                     }
                 })
-
                 this.setActiveKey(currentKey)
             }
         }
@@ -87,7 +68,6 @@ class TabsContainers extends React.Component {
     // 配置 actions / 发起 actions
     setActions = (strKey, arrPanes) => {
         const previousState = cloneDeep([...this.props.panesState.panes, arrPanes]) // 深拷贝 => 将数组带入 addObj
-
         this.props.onAddPane({
             activeKey: strKey,
             panes: previousState
@@ -120,32 +100,23 @@ class TabsContainers extends React.Component {
         let currentKey // 如果要删除的标签是当前展示的, 删除后更新key 为当前位置(currentIndex)的 key
         let currentUrl // 如果要删除的标签是当前展示的, 删除后 更改url
 
-        // 返回一个符合条件的 数组
         const currentPanes = this.state.panes.filter(pane => pane.key !== targetKey)
 
         if (targetKey === activeKey) {
-            // console.log('删除的是展示的标签, activeKey要改动')
-
             this.state.panes.forEach((pane, i) => {
                 if (pane.key === targetKey) {
-                    currentIndex = i // 要删除的key === 展示的key => 要删除的 i 下标位置保存到 currentIndex(删除pane数组中当前对象; 然后到返回的数组中查找下标为 i的新对象, 拿出他的key 作为最新展示的key)
+                    currentIndex = i
                 }
             })
-
-            // 设置删除后 最新的 展示 Key 与 要跳转的 url
             currentKey = currentPanes[currentIndex - 1].key
             currentUrl = currentPanes[currentIndex - 1].path
-
             this.props.onAddPane({
                 activeKey: cloneDeep(currentKey),
                 panes: cloneDeep(currentPanes)
             })
-
             this.props.tabsProps.history.push(currentUrl)
         } else {
-            // console.log('删除的不是展示的, activeKey不改动; 不跳路由')
             currentKey = activeKey
-
             this.props.onAddPane({
                 activeKey: cloneDeep(currentKey),
                 panes: cloneDeep(currentPanes)
@@ -155,7 +126,6 @@ class TabsContainers extends React.Component {
 
     // 当 props改变时 触发 => 调用 更改 setState的方法
     componentWillReceiveProps = (nextProps) => {
-        // console.log('store 发生改变')
         let currentState = cloneDeep(nextProps.panesState)
         this.setState(currentState)
     }
