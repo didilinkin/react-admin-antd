2e11222b08fea0e1dcea5cae3effbb7c63a91cda
yanxiaodi <929213769@qq.com>
2017-08-17 17:19:05 +0800

    :bug: 将浏览器检测代码放入html

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import React from 'react'
 import { render } from 'react-dom'
 
 import { authenticate } from './utils/LocalStore'
-import { myBrowser } from './utils'
 import { configureStore, history } from './store/configureStore/index'
 import Root from './common/containers/Root'
 
@@ -11,12 +10,6 @@ import 'babel-polyfill'
 
 const store = configureStore()
 const authState = authenticate()
-
-const versions = myBrowser()
-
-if (versions === 'IE55' || versions === 'IE6' || versions === 'IE7' || versions === 'IE8') {
-    history.push('/navigator')
-}
 
 render(
     <Root
