5deb22bd4fce9e1db7c151eb7863277410722924
yanxiaodi <929213769@qq.com>
2017-08-29 14:06:57 +0800

    :art: 添加路由 '投诉建议' 与 '整改通知;'

---
+++ src/modules/CLIENT/containers/Complain.jsx
@@ -0,0 +1,12 @@
+// 客户管理 - 投诉建议
+import React from 'react'
+
+class Complain extends React.Component {
+    render () {
+        return (
+            <h1> 客户管理 - 投诉建议 </h1>
+        )
+    }
+}
+
+export default Complain
---
+++ src/modules/CLIENT/containers/PropertyNotice.jsx
@@ -0,0 +1,12 @@
+// 客户管理 - 物业公告
+import React from 'react'
+
+class PropertyNotice extends React.Component {
+    render () {
+        return (
+            <h1> 客户管理 - 物业公告 </h1>
+        )
+    }
+}
+
+export default PropertyNotice
