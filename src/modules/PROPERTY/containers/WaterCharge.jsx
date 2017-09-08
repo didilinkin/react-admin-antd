42b50f7e7978cae18aace7ae1f6e76454ea0775e
liuzhiqiang <liuzhiqiang@forevercj.com>
2017-09-08 14:43:52 +0800

    select 可清除

--- src/modules/PROPERTY/containers/WaterCharge.jsx
+++ src/modules/PROPERTY/containers/WaterCharge.jsx
@@ -111,7 +111,7 @@ class ChargeWaterBill extends React.Component {
             message: data.data,
             icon: <Icon type="smile-circle" style={{color: '#108ee9'}} />
         })
-        this.refreshTwo(1)
+        this.refresh({}, {}, {})
     }
     info = (id) => {
         this.setState({
@@ -129,7 +129,7 @@ class ChargeWaterBill extends React.Component {
             message: data.data,
             icon: <Icon type="smile-circle" style={{color: '#108ee9'}} />
         })
-        this.refreshTwo(1)
+        this.refresh({}, {}, {})
     }
     async initialRemarks () {
         this.setState({loading: true})
